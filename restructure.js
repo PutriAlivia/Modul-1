d159d682401cec772d770f990e7d636f27d500d9
Putri Alivia <putrialivia666@gmail.com>
2022-02-17 14:10:31 +0700

    Membuat Restructure

---
+++ restructure.js
@@ -0,0 +1,8 @@
+var mahasiswiPolinema = {
+    name: 'Putri',
+    height: '160',
+    output(){
+        alert(`Mbak ${this.name} tingginya ${this.height} cm`);
+    }
+};
+mahasiswiPolinema.output();
